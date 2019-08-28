a2d21080ea19495cf7017a0aca1027957c4dbbbe
Jimmy Andersson <jimmyandersson89@gmail.com>
2019-08-28 21:53:45 +0100

    The text for the Me-page section is done, started with some style

--- src/App.vue
+++ src/App.vue
@@ -4,35 +4,106 @@
         <HelloWorld msg="Welcome to Your Vue.js App" />
         <Me />-->
         <div class="nav">
-            <router-link to="/">Me</router-link>
-            <router-link to="/reports/week/1">Report</router-link>
+            <ul>
+                <li>
+                    <router-link to="/">Me</router-link>
+                </li>
+                <li>
+                    <router-link to="/reports/week/1">Report</router-link>
+                </li>
+            </ul>
         </div>
         <router-view></router-view>
+        <footer class="site-footer">
+            <p>Copyright &copy; 2018 Jimmy Andersson</p>
+        </footer>
     </div>
 </template>
 
 <script>
 // import HelloWorld from "./components/HelloWorld.vue";
-import Me from "./components/Me.vue";
-import Report from "./components/Report.vue";
+// import Me from "./components/Me.vue";
+// import Report from "./components/Report.vue";
 
 export default {
     name: "app",
     components: {
         // HelloWorld,
-        Me,
-        Report
+        // Me,
+        // Report
     }
 };
 </script>
 
 <style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+html {
+    font-size: 100%;
+}
+
+.container {
+    max-width: 1100px;
+    margin: auto;
+    padding: 1rem 3rem;
+    min-height: 78vh;
+}
+
 #app {
     font-family: "Avenir", Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
+    /* margin-top: 60px; */
+}
+
+.nav {
+    height: 40px;
+    width: 100%;
+    background: #ddd;
+}
+
+.nav ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    text-align: center;
+    background: #444;
+}
+
+.nav li {
+    display: inline-block;
+    width: 120px;
+    height: 40px;
+    line-height: 40px;
+    font-size: 1em;
+    color: #fff;
+    margin-right: -4px;
+}
+
+.nav a {
+    display: block;
+    text-decoration: none;
+    color: #fff;
+}
+
+.nav a:hover {
+    background-color: #222;
+    color: rgb(252, 92, 0);
+}
+
+.site-footer {
+    width: 100%;
+    height: 100px;
+    background-color: #444;
+    padding-top: 20px;
+    text-align: center;
+    margin-top: 20px;
+    color: #fff;
 }
 </style>
--- src/components/Me.vue
+++ src/components/Me.vue
@@ -1,12 +1,29 @@
 <template>
-    <h1>Me sida</h1>
+    <div class="container">
+        <h1>Me sida</h1><img :src="image" alt= />
+        <h2>Bio:</h2>
+        <p>Jag bor i Skövde tillsammans med min sambo Martina och vår hund Bella (Se bild). På fritiden sitter jag gärna vid datorn och kollar filmer och serier, blir att jag spelar lite då och då. När jag inte sitter vid datorn umgås jag med familj och vänner, spelar lite beachvolleyboll och golf. Blir några skogspromenader när jag spelar golf då det verkar vara något fel på mina golfbollar, de drar sig mot skogen.</p>
+        <p>På senare tid har det blivit mer aktiviteter ute eftersom vi skaffat hund. Det är en av anledningarna jag ville börja studera på distans, så jag kan vara hemma med Bella.</p>
+        <p>En annan anledning till att jag sökte programmet Webbprogrammering är att jag vill lära mig mer om webbutveckling. Jag har tidigare läst ett tekniskt gymnasieutbildning med lite grundläggande programmering i C++ och PHP, kommer inte ihåg mycket av det. Efter lumpen och lite jobb började jag en 3-årig högskoleutbildning där jag fick lära mig lite programmering i C++ och Perl.</p>
+        <p>Efter högskolan har jag jobbat som IT-tekniker och onsite-tekniker. På fritiden har jag lärt mig själv lite programmering, något som jag tycker är roligt och ser fram emot att lära mig mer om.</p>
+    </div>
 </template>
 
 <script>
 export default {
-    name: "Me"
+    name: "Me",
+    data() {
+        return {
+            image: require("@/assets/me_bella.jpg")
+        };
+    }
 };
 </script>
 
-<style>
+<style scoped>
+img {
+    width: 100%;
+    max-width: 500px;
+    height: auto;
+}
 </style>
